714f26e6dd5644982c14f2f1b5551c21e91c8191
stefanoSAN23 <stefano.spotify23@gmail.com>
2023-11-29 14:23:17 -0600

    se añadio el add-dish

--- admin/add-dish.php
+++ admin/add-dish.php
@@ -9,21 +9,22 @@
      require_once '../database.php';
 
      // Reference: https://medoo.in/api/select
-     $states = $database->select("tb_us_states","*");
+     
 
      // Reference: https://medoo.in/api/select
-     $categories = $database->select("tb_camping_categories","*");
+     $categories = $database->select("tb_for_categories","*");
+     $categories_groups = $database->select("tb_categories_groups","*");
                         
      $message="";
 
      if ($_POST){
-        if (isset($_FILES["destination_image"])){
+        if (isset($_FILES["dish_image"])){
             $errors = [];
-            $file_name = $_FILES["destination_image"]["name"];
-            $file_size = $_FILES["destination_image"]["size"];
-            $file_tmp = $_FILES["destination_image"]["tmp_name"];
-            $file_type = $_FILES["destination_image"]["type"];
-            $file_ext_arr = explode(".", $_FILES["destination_image"]["name"]);
+            $file_name = $_FILES["dish_image"]["name"];
+            $file_size = $_FILES["dish_image"]["size"];
+            $file_tmp = $_FILES["dish_image"]["tmp_name"];
+            $file_type = $_FILES["dish_image"]["type"];
+            $file_ext_arr = explode(".", $_FILES["dish_image"]["name"]);
 
             $file_ext = end($file_ext_arr);
             $img_ext = ["jpge", "png", "jpg", "webp"];
@@ -34,25 +35,23 @@
             }
 
             if(empty($errors)){
-                $filename = strtolower($_POST["destination_lname"]);
+                $filename = strtolower($_POST["dish_name"]);
                 $filename = str_replace(',', '', $filename);
                 $filename = str_replace('.', '', $filename);
                 $filename = str_replace(' ', '-', $filename);
-                $img = "location-".$filename.".".$file_ext;
+                $img = "dish-".$filename.".".$file_ext;
                 
                 move_uploaded_file($file_tmp, "../imgs/".$img);
 
-                $database->insert("tb_destinations",[
-                    "id_us_state" => $_POST["destination_state"],
-                    "id_camping_category" => $_POST["category"],
-                    "destination_lname" => $_POST["destination_lname"],
-                    "destination_lname_tr" => $_POST["destination_lname_tr"],
-                    "destination_sname" => $_POST["destination_sname"],
-                    "destination_sname_tr" => $_POST["destination_sname_tr"],
-                    "destination_description" => $_POST["destination_description"],
-                    "destination_description_tr" => $_POST["destination_description_tr"],
-                    "destination_image" => $img,
-                    "destination_price" => $_POST["destination_price"]
+                $database->insert("tb_for_dishes",[
+                    "id_category"=>$_POST["name_category"],
+                    "id_category_group"=>$_POST["category_group_name"],
+                    "dish_name"=>$_POST["dish_name"],
+                    "dish_name_chinese"=>$_POST["dish_name_chinese"],
+                    "dish_description"=>$_POST["dish_description"],
+                    "dish_description_chinese"=>$_POST["dish_description_chinese"],
+                    "dish_image"=> $img,
+                    "dish_price"=>$_POST["dish_price"],
                 ]);
             }
         }
@@ -67,68 +66,66 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Add Destination</title>
+    <title>Add Dish</title>
     <link rel="stylesheet" href="../css/themes/admin.css">
 </head>
 <body>
     <div class="container">
-        <h2>Add New Destination</h2>
-        <form method="post" action="add-destination.php" enctype="multipart/form-data">
+        <h2>Add New Dish</h2>
+        <form method="post" action="add-dish.php" enctype="multipart/form-data">
             <div class="form-items">
-                <label for="destination_lname">Destination Name</label>
-                <input id="destination_lname" class="textfield" name="destination_lname" type="text">
+                <label for="dish_name">Dish Name</label>
+                <input id="dish_name" class="textfield" name="dish_name" type="text">
             </div>
             <div class="form-items">
-                <label for="destination_lname_tr">Destination Name - TR</label>
-                <input id="destination_lname_tr" class="textfield" name="destination_lname_tr" type="text">
+                <label for="dish_name_chinese">Dish name - CH</label>
+                <input id="dish_name_chinese" class="textfield" name="dish_name_chinese" type="text">
             </div>
+
             <div class="form-items">
-                <label for="destination_sname">Destination Short Name</label>
-                <input id="destination_sname" class="textfield" name="destination_sname" type="text">
-            </div>
-            <div class="form-items">
-                <label for="destination_sname_tr">Destination Short Name - TR</label>
-                <input id="destination_sname_tr" class="textfield" name="destination_sname_tr" type="text">
-            </div>
-            <div class="form-items">
-                <label for="destination_state">Destination State</label>
-                <select name="destination_state" id="destination_state">
-                    <?php 
-                        foreach ($states as $state){
-                            echo "<option value=".$state["id_us_state"].">".$state["us_state_name"]."</option>";
-                        }
-                    ?>
-                </select>
-            </div>
-            <div class="form-items">
-                <label for="destination_category">Destination Category</label>
-                <select name="category" id="destination_category">
+                <label for="name_category">Dish Category</label>
+                <select name="category" id="name_category">
                     <?php 
                         foreach ($categories as $category){
-                            echo "<option value=".$category["id_camping_category"].">".$category["camping_category_name"]."</option>";
+                            echo "<option value=".$category["id_category"].">".$category["name_category"]."</option>";
                         }
                     ?>
                 </select>
             </div>
             <div class="form-items">
-                <label for="destination_description">Destination Description</label>
-                <textarea id="destination_description" name="destination_description" id="" cols="30" rows="10"></textarea>
+                <label for="category_group_name">Dish Category Group</label>
+                <select name="category_group_name" id="category_group_name">
+                    <?php 
+                    var_dump($categories_groups);
+                        foreach($categories_groups as $category_group){
+                            if($item[0]["id_category_group"] == $category_group["id_category_group"]){
+                                echo "<option value='".$category_group["id_category_group"]."' selected>".$category_group["category_group_name"]."</option>";
+                            }else{
+                                echo "<option value='".$category_group["id_category_group"]."'>".$category_group["category_group_name"]."</option>";
+                            }
+                        }
+                    ?>
+                </select>
             </div>
             <div class="form-items">
-                <label for="destination_description_tr">Destination Description - TR</label>
-                <textarea id="destination_description_tr" name="destination_description_tr" id="" cols="30" rows="10"></textarea>
+                <label for="dish_description">Dish Description</label>
+                <textarea id="dish_description" name="dish_description" id="" cols="30" rows="10"></textarea>
             </div>
             <div class="form-items">
-                <label for="destination_image">Destination Image</label>
+                <label for="dish_description_chinese">Dish Description - CH</label>
+                <textarea id="dish_description_chinese" name="dish_description_chinese" id="" cols="30" rows="10"></textarea>
+            </div>
+            <div class="form-items">
+                <label for="dish_image">Dish Image</label>
                 <img id="preview" src="../imgs/destination-placeholder.webp" alt="Preview">
-                <input id="destination_image" type="file" name="destination_image" onchange="readURL(this)">
+                <input id="dish_image" type="file" name="dish_image" onchange="readURL(this)">
             </div>
             <div class="form-items">
-                <label for="destination_price">Destination Price</label>
-                <input id="destination_price" class="textfield" name="destination_price" type="text">
+                <label for="dish_price">Dish Price</label>
+                <input id="dish_price" class="textfield" name="dish_price" type="text">
             </div>
             <div class="form-items">
-                <input class="submit-btn" type="submit" value="Add New Destination">
+                <input class="submit-btn" type="submit" value="Add New Dish">
             </div>
         </form>
     </div>
